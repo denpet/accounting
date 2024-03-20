c1a740fce329fb3aac5c1fa561448bb297825d34
Dennis Pettersson <dennis@eden.ph>
2024-03-20 14:09:05 +0800

    Change default route

--- api/routes/web.php
+++ api/routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
+Route::view('{any}', 'app')->where('any', '.*');
+
+// Route::get('/', function () {
+//     return ['Laravel' => app()->version()];
+// });
