6bc16fbb43ac9cf0fe5ab1e3c767edb0d7df90c2
Dennis Pettersson <dennis@eden.ph>
2024-10-14 06:05:36 +0200

    Add Unionbank to accounts

--- app/src/components/Accounting/Transaction/ErEdit.vue
+++ app/src/components/Accounting/Transaction/ErEdit.vue
@@ -153,14 +153,15 @@ onMounted(() => {
 
 const fromAccountOptions = computed(() => {
   return accountStore.options.filter((value: { value: number }) => {
-    return [1, 3].includes(value.value)
+    return [1, 3, 38, 39].includes(value.value)
   })
 })
 
 const toAccountOptions = computed(() => {
   return accountStore.options.filter((value: { value: number }) => {
     return [
-      9, 10, 11, 12, 13, 15, 19, 20, 22, 23, 28, 29, 44, 48, 49, 51, 53, 54,
+      9, 10, 11, 12, 13, 15, 19, 20, 22, 23, 28, 29, 38, 39, 44, 48, 49, 51, 53,
+      54,
     ].includes(value.value)
   })
 })
