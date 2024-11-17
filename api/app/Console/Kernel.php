d93bf66e7fb519bfdb6a5345dcb0db128438777f
Dennis Pettersson <dennis@eden.ph>
2024-11-17 16:31:08 +0800

    Add schedule to import cash

--- api/app/Console/Kernel.php
+++ api/app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('zkteco:sync')->everyTenMinutes();
+        $schedule->command('unicenta:import-close-cash')->dailyAt('02:00');
     }
 
     /**
